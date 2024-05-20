d39dad370a1022536a45fe6dc46e77420e2a9dee
anushka3107-bit <bhardwajanushka180@gmail.com>
2024-05-20 16:29:37 +0530

    created all components

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import LandingPage from "./Components/LandingPage";
 import EventMainPage from "./Components/EventLandingPage/EventMainPage";
 import EventInfoStrip from "./Components/EventLandingPage/EventInfoStrip";
 import ExploreEventPage from "./Components/EventLandingPage/ExploreEventPage";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 import './App.css';
 
 const App = () => {
   return (
     <>
-      {/* <LandingPage /> */}
-      {/* <EventMainPage /> */}
-      <div className="twoPage">
+      <LandingPage />
+      <div className="mainPage">
+      <EventMainPage />
       <EventInfoStrip />
       <ExploreEventPage />
       </div>
-     
     </>
   );
 };
--- src/Components/EventLandingPage/EventMainPage.js
+++ src/Components/EventLandingPage/EventMainPage.js
@@ -1,95 +1,70 @@
-// import React from "react";
-// import "../../style/eventPage.css";
-// import Branding from "../../Images/Astrix Branding.png";
-// import Logo from "../../Images/Logo.png";
-// import EventBg from "../../Images/ASTRIX EVENTS.png";
-// import Slider from "react-slick";
-
-// const EventMainPage = () => {
-  
-//   var settings = {
-//     dots: false,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1
-//   };
-
-//   return (
-//     <div className="eventMainPage">
-//       {/* header */}
-//       <div className="header-logo">
-//         <img src={Branding} alt="Branding" className="branding" />
-//         <h1 className="logo">Astrix.</h1>
-//       </div>
-//       {/* inner banner */}
-//       <div className="inner-banner">
-//         <h1 className='header-one'>ASTR<br />IX</h1>
-//         <h1 className='header-two'>EVE<br />NTS</h1>
-//         {/* <img src={EventBg} alt="event bg" className="eventBg" /> */}
-//       </div>
-
-//       {/* event image carousel */}
-//       <div>
-//         <img src="https://unsplash.com/photos/group-of-people-attending-to-concert-9tZhyQskezA" alt="" />
-//         <img src="https://unsplash.com/photos/group-of-people-attending-to-concert-9tZhyQskezA" alt="" />
-//         <img src="https://unsplash.com/photos/group-of-people-attending-to-concert-9tZhyQskezA" alt="" />
-//       </div>
-
-
-//     </div>
-//   );
-// };
-
-// export default EventMainPage;
-
-
-import React from "react";
-import "../../style/eventPage.css";
-import Branding from "../../Images/Astrix Branding.png";
-import Slider from "react-slick";
+import React, { useRef } from 'react';
+import Slider from 'react-slick';
+import '../../style/eventPage.css';
+import Logo from '../../Images/Astrix Branding.png';
+import EventBg from "../../Images/ASTRIX EVENTS.png";
+import Frame1 from "../../Images/Frame 1261154670.png";
+import Frame2 from "../../Images/Frame 1261154671.png";
+import Frame3 from "../../Images/Frame 1261154672.png";
+import Frame4 from "../../Images/Frame 1261154671 (1).png";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const EventMainPage = () => {
+  const sliderRef = useRef(null);
 
-  var settings = {
-    dots: false,
+  const settings = {
+    dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToShow: 1.2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    beforeChange: (current, next) => {
+      if (next === sliderRef.current.props.children.length - 1) {
+        setTimeout(() => {
+          sliderRef.current.slickGoTo(0);
+        }, 1000); 
+      }
+    }
   };
 
   return (
-    <div className="eventMainPage">
-      {/* header */}
-      <div className="headerLogo">
-        <img src={Branding} alt="Branding" className="branding" />
-        <h1 className="logo">Astrix.</h1>
-      </div>
+    <div className='eventMainPage'>
+    <div className='header-logo'>
+          <img src={Logo} 
+            alt='logo' className='logo'/>
+          <h1 className='heading'>Astrix.</h1>
+        </div>
+  
+        <div className='inner-banner'>
+          <img 
+            src={EventBg}
+            alt='event bg'
+            className='eventBg'
+          />
+        </div> 
 
-      {/* inner banner */}
-      <div className="innerBanner">
-        <h1 className='headerOne'>ASTR<br />IX</h1>
-        <h1 className='headerTwo'>EVE<br />NTS</h1>
-      </div>
-
-      {/* event image carousel */}
-      <div className="imageCarousel">
-        <Slider {...settings}>
-          <div>
-            <img src="https://images.unsplash.com/photo-1593642532973-d31b6557fa68" alt="Event 1" />
-          </div>
-          <div>
-            <img src="https://images.unsplash.com/photo-1581349484269-baa01df3e073" alt="Event 2" />
-          </div>
-          <div>
-            <img src="https://images.unsplash.com/photo-1573497491208-6b1acb260507" alt="Event 3" />
-          </div>
-        </Slider>
-      </div>
+        <div className='slider'>
+          <Slider {...settings} ref={sliderRef}>
+            <div>
+              <img src={Frame1} alt="frame1"/>
+            </div>
+            <div>
+              <img src={Frame2} alt="frame2"/>
+            </div>
+            <div>
+              <img src={Frame3} alt="frame3"/>
+            </div>
+            <div>
+              <img src={Frame4} alt="frame4"/>
+            </div>
+          </Slider>
+        </div>
     </div>
   );
-};
+}
 
 export default EventMainPage;
-
